86240fb017343283ed968ae22f0b16bd29ffe2dc
Timothy Palpant <tim@palpant.us>
2018-02-26 10:20:17 -0500

    Use a sync.Pool of []float64 to reduce allocations for ExpectedWork

--- optimization/expected_work.go
+++ optimization/expected_work.go
@@ -3,9 +3,16 @@ package optimization
 import (
 	"encoding/gob"
 	"fmt"
+	"sync"
 
 	"github.com/timpalpant/yahtzee"
 )
+
+var valuesPool = sync.Pool{
+	New: func() interface{} {
+		return make([]float64, yahtzee.MaxScore)
+	},
+}
 
 func init() {
 	gob.Register(ExpectedWork{})
@@ -16,8 +23,6 @@ func init() {
 type ExpectedWork struct {
 	// The expected work at the start of a game.
 	E0 float64
-	// The smallest integer such that W(start) > 0.
-	Start int
 	// [W(start + 1), W(start + 2), ..., W(start + N)]
 	// where W(start + N) is the largest integer such that W(start + N) < E0.
 	Values []float64
@@ -28,21 +33,19 @@ func NewExpectedWork(e0 float64) ExpectedWork {
 }
 
 func (ew ExpectedWork) Copy() GameResult {
-	var vCopy []float64
-	if len(ew.Values) > 0 {
-		vCopy = make([]float64, len(ew.Values))
-		copy(vCopy, ew.Values)
+	values := valuesPool.Get().([]float64)
+	for i := range values {
+		values[i] = ew.E0
 	}
 
 	return ExpectedWork{
 		E0:     ew.E0,
-		Start:  ew.Start,
-		Values: vCopy,
+		Values: values,
 	}
 }
 
 func (ew ExpectedWork) Zero() GameResult {
-	values := make([]float64, yahtzee.MaxScore)
+	values := valuesPool.Get().([]float64)
 	for i := range values {
 		values[i] = 1
 	}
@@ -53,58 +56,32 @@ func (ew ExpectedWork) Zero() GameResult {
 	}
 }
 
-func (ew ExpectedWork) Stop() int {
-	return ew.Start + len(ew.Values)
-}
-
-func (ew ExpectedWork) GetValue(score int) float64 {
-	if score <= ew.Start {
-		return 0
-	}
-
-	idx := score - ew.Start
-	if idx >= len(ew.Values) {
-		return ew.E0
-	}
-
-	return ew.Values[idx]
-}
-
 func (ew ExpectedWork) String() string {
-	return fmt.Sprintf("{Start: %v, Stop: %v, Dist: %v}",
-		ew.Start, ew.Stop(), ew.Values)
+	return fmt.Sprintf("{Dist: %v}", ew.Values)
+}
+
+func clear(v []float64) {
+	for i := range v {
+		v[i] = 0
+	}
 }
 
 func (ew ExpectedWork) Max(gr GameResult) GameResult {
 	other := gr.(ExpectedWork)
-	newStart := ew.Start
-	if other.Start > newStart {
-		newStart = other.Start
+	newValues := valuesPool.Get().([]float64)
+	clear(newValues)
+	for s := 0; s < len(newValues); s++ {
+		x1 := ew.Values[s]
+		x2 := other.Values[s]
+		newValues[s] = min(x1, x2)
 	}
 
-	newStop := ew.Stop()
-	if other.Stop() > newStop {
-		newStop = other.Stop()
+	if len(ew.Values) == yahtzee.MaxScore {
+		valuesPool.Put(ew.Values)
 	}
 
-	newValues := make([]float64, newStop-newStart)
-	for s := newStart; s < newStop; s++ {
-		x1 := ew.GetValue(s)
-		x2 := other.GetValue(s)
-		newValues[s-newStart] = min(x1, x2)
-
-		// NOTE: Uses the fact that expected work is monotonic.
-		if newValues[s-newStart] >= ew.E0 {
-			newValues = newValues[:s-newStart]
-			break
-		}
-	}
-
-	return ExpectedWork{
-		E0:     ew.E0,
-		Start:  newStart,
-		Values: newValues,
-	}
+	ew.Values = newValues
+	return ew
 }
 
 func min(x1, x2 float64) float64 {
@@ -117,35 +94,28 @@ func min(x1, x2 float64) float64 {
 
 func (ew ExpectedWork) Add(gr GameResult, weight float64) GameResult {
 	other := gr.(ExpectedWork)
-	newStart := ew.Start
-	if other.Start < newStart {
-		newStart = other.Start
+	newValues := valuesPool.Get().([]float64)
+	copy(newValues, ew.Values)
+	for s := 0; s < len(newValues); s++ {
+		newValues[s] += weight * other.Values[s]
 	}
 
-	newStop := yahtzee.MaxScore
-	newValues := make([]float64, newStop-newStart)
-	copy(newValues[ew.Start-newStart:], ew.Values)
-	for s := other.Start; s < yahtzee.MaxScore; s++ {
-		newValues[s-newStart] += weight * other.GetValue(s)
-
-		// NOTE: Uses the fact that expected work is monotonic.
-		if newValues[s-newStart] >= ew.E0 {
-			newValues = newValues[:s-newStart]
-			break
-		}
-	}
-
-	return ExpectedWork{
-		E0:     ew.E0,
-		Start:  ew.Start,
-		Values: newValues,
-	}
+	valuesPool.Put(ew.Values)
+	ew.Values = newValues
+	return ew
 }
 
 func (ew ExpectedWork) Shift(offset int) GameResult {
-	return ExpectedWork{
-		E0:     ew.E0,
-		Start:  ew.Start + offset,
-		Values: ew.Values,
+	newValues := valuesPool.Get().([]float64)
+	for i := 0; i < offset; i++ {
+		newValues[i] = 0
 	}
+
+	for s := offset; s < len(newValues); s++ {
+		newValues[s] = ew.Values[s-offset]
+	}
+
+	valuesPool.Put(ew.Values)
+	ew.Values = newValues
+	return ew
 }
