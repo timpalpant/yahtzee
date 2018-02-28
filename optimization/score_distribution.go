0a27aab00808181c02c209614c226de72a0d36af
Timothy Palpant <tim@palpant.us>
2018-02-27 22:11:29 -0500

    Apply learnings from ExpectedWork to ScoreDistribution

--- optimization/score_distribution.go
+++ optimization/score_distribution.go
@@ -2,128 +2,66 @@ package optimization
 
 import (
 	"encoding/gob"
-	"fmt"
+	"sync"
+
+	"github.com/timpalpant/yahtzee"
+	"github.com/timpalpant/yahtzee/optimization/f32"
 )
+
+var sdPool = sync.Pool{
+	New: func() interface{} {
+		return make(ScoreDistribution, yahtzee.MaxScore)
+	},
+}
 
 func init() {
 	gob.Register(ScoreDistribution{})
 }
 
 // ScoreDistribution implements GameResult, and represents
-// maximizing your probability of obtaining score s for all s.
-type ScoreDistribution struct {
-	// The largest integer such that P(start) == 1.
-	Start int
-	// [P(start + 1), P(start + 2), ..., P(start + N)]
-	// where P(start + N) is the largest integer such that P(s1) > 0.
-	Probabilities []float32
-}
+// minimizing the work required to achieve a desired score.
+type ScoreDistribution []float32
 
 func NewScoreDistribution() ScoreDistribution {
-	return ScoreDistribution{}
+	sd := sdPool.Get().(ScoreDistribution)
+	for i := range sd {
+		sd[i] = 0
+	}
+
+	return sd
 }
 
-func (sd ScoreDistribution) Close() {}
+func (sd ScoreDistribution) Close() {
+	sdPool.Put(sd)
+}
 
 func (sd ScoreDistribution) Copy() GameResult {
-	pCopy := make([]float32, len(sd.Probabilities))
-	copy(pCopy, sd.Probabilities)
-
-	return ScoreDistribution{
-		Start:         sd.Start,
-		Probabilities: pCopy,
-	}
+	newSD := sdPool.Get().(ScoreDistribution)
+	copy(newSD, sd)
+	return newSD
 }
 
 func (sd ScoreDistribution) Zero() GameResult {
 	return NewScoreDistribution()
 }
 
-func (sd ScoreDistribution) Stop() int {
-	return sd.Start + len(sd.Probabilities)
-}
-
-func (sd ScoreDistribution) GetProbability(score int) float32 {
-	if score <= sd.Start {
-		return 1
-	}
-
-	idx := score - sd.Start
-	if idx >= len(sd.Probabilities) {
-		return 0
-	}
-
-	return sd.Probabilities[idx]
-}
-
-func (sd ScoreDistribution) String() string {
-	return fmt.Sprintf("{Start: %v, Stop: %v, Dist: %v}",
-		sd.Start, sd.Stop(), sd.Probabilities)
-}
-
 func (sd ScoreDistribution) Max(gr GameResult) GameResult {
 	other := gr.(ScoreDistribution)
-	maxStart := sd.Start
-	if other.Start > maxStart {
-		maxStart = other.Start
-	}
-
-	maxStop := sd.Stop()
-	if other.Stop() > maxStop {
-		maxStop = other.Stop()
-	}
-
-	newProbabilities := make([]float32, maxStop-maxStart)
-	for s := maxStart; s < maxStop; s++ {
-		//x1 := sd.Probabilities[s-sd.Start]
-		//x2 := other.Probabilities[s-other.Start]
-		x1 := sd.GetProbability(s)
-		x2 := other.GetProbability(s)
-		newProbabilities[s-maxStart] = max(x1, x2)
-	}
-
-	return ScoreDistribution{
-		Start:         maxStart,
-		Probabilities: newProbabilities,
-	}
-}
-
-func max(x1, x2 float32) float32 {
-	if x1 > x2 {
-		return x1
-	}
-
-	return x2
+	f32.Min(sd, other)
+	return sd
 }
 
 func (sd ScoreDistribution) Add(gr GameResult, weight float32) GameResult {
 	other := gr.(ScoreDistribution)
-	// other.Start must be > sd.Start since otherwise we will be adding
-	// to a probability that is already 1.
-	if other.Start < sd.Start {
-		panic(fmt.Errorf("adding will create probability > 1: %v <= %v", other.Start, sd.Start))
-	}
-
-	maxStop := sd.Stop()
-	if other.Stop() > maxStop {
-		maxStop = other.Stop()
-	}
-
-	newProbabilities := make([]float32, maxStop-sd.Start)
-	copy(newProbabilities, sd.Probabilities)
-	for s := sd.Start; s < other.Stop(); s++ {
-		newProbabilities[s-sd.Start] += weight * other.GetProbability(s)
-	}
-
-	return ScoreDistribution{
-		Start:         sd.Start,
-		Probabilities: newProbabilities,
-	}
+	f32.AddScaled(sd, weight, other)
+	return sd
 }
 
 func (sd ScoreDistribution) Shift(offset int) GameResult {
-	return ScoreDistribution{
-		Start:         sd.Start + offset,
-		Probabilities: sd.Probabilities,
+	newSD := sdPool.Get().(ScoreDistribution)
+	for i := 0; i < offset; i++ {
+		newSD[i] = 0
 	}
+	copy(newSD[offset:], sd)
+	return newSD
 }
